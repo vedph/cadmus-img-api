b1d8d9e626403e53fc01717f6efde172422c9ade
Myrmex <dfusi@hotmail.com>
2023-05-16 09:58:55 +0200

    Initial commit

---
+++ CadmusImgApi/Services/ImgRepositoryProvider.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Core.Storage;
+using Cadmus.Mongo;
+using Cadmus.General.Parts;
+using Cadmus.Philology.Parts;
+using Cadmus.Img.Parts;
+
+namespace Cadmus.Img.Services;
+
+/// <summary>
+/// Cadmus Img repository provider.
+/// </summary>
+/// <seealso cref="IRepositoryProvider" />
+public sealed class ImgRepositoryProvider : IRepositoryProvider
+{
+    private readonly IPartTypeProvider _partTypeProvider;
+
+    /// <summary>
+    /// The connection string.
+    /// </summary>
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ImgRepositoryProvider"/>
+    /// class.
+    /// </summary>
+    public ImgRepositoryProvider()
+    {
+        ConnectionString = "";
+        TagAttributeToTypeMap map = new();
+        map.Add(new[]
+        {
+            // Cadmus.General.Parts
+            typeof(NotePart).GetTypeInfo().Assembly,
+            // Cadmus.Philology.Parts
+            typeof(ApparatusLayerFragment).GetTypeInfo().Assembly,
+            // Cadmus.Img.Parts
+            typeof(GalleryImageAnnotationsPart).GetTypeInfo().Assembly,
+        });
+
+        _partTypeProvider = new StandardPartTypeProvider(map);
+    }
+
+    /// <summary>
+    /// Gets the part type provider.
+    /// </summary>
+    /// <returns>part type provider</returns>
+    public IPartTypeProvider GetPartTypeProvider()
+    {
+        return _partTypeProvider;
+    }
+
+    /// <summary>
+    /// Creates a Cadmus repository.
+    /// </summary>
+    /// <returns>repository</returns>
+    public ICadmusRepository CreateRepository()
+    {
+        // create the repository (no need to use container here)
+        MongoCadmusRepository repository = new(_partTypeProvider,
+                new StandardItemSortKeyBuilder());
+
+        repository.Configure(new MongoCadmusRepositoryOptions
+        {
+            ConnectionString = ConnectionString ??
+            throw new InvalidOperationException(
+                "No connection string set for IRepositoryProvider implementation")
+        });
+
+        return repository;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Cadmus Imaging API
+
+Quick Docker image build:
+
+    docker build . -t vedph2020/cadmus-img-api:0.0.1 -t vedph2020/cadmus-img-api:latest
+
+(replace with the current version).
+
+This is a Cadmus API layer used to test the Cadmus imaging frontend components in their [shell](https://github.com/vedph/cadmus-img-shell).
+
+## History
+
+- 2023-05-16: updated packages.
